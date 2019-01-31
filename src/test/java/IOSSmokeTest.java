adc10cda8a35d30226e5e125efdc23fb90a70bb6
tozolino <tozolino@yahoo.co.uk>
2019-01-31 18:23:28 +0100

    added isInstalled and isUninstalled tests for iOS

--- src/test/java/IOSSmokeTest.java
+++ src/test/java/IOSSmokeTest.java
@@ -1,39 +1,61 @@
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 public class IOSSmokeTest {
 
-        IOSDesiredCaps caps;
-        IOSDriver<IOSElement> driver;
+    IOSDesiredCaps caps;
+    IOSDriver driver;
+    HashMap<String, Object> params;
 
 
-        @BeforeClass
-        public void beforeClass() throws InterruptedException, MalformedURLException {
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
 
-            URL url = new URL("http://0.0.0.0:4723/wd/hub");
-            caps = new IOSDesiredCaps();
-            driver = new IOSDriver<IOSElement>(url, caps.setupIOSDesirecCapabilities());
-        }
-
-        @Test
-        public void test() throws InterruptedException {
-
-            Thread.sleep(3000);
-
-
-
-        }
-
-        @AfterClass
-        public void afterClass(){
-            driver.quit();
-        }
-
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+        caps = new IOSDesiredCaps();
+        driver = new IOSDriver<IOSElement>(url, caps.setupIOSDesirecCapabilities());
+        params = new HashMap<String,Object>();
+        params.put("bundleId", "uxbae.MacAndCheese");
     }
+
+    @Test
+    public void testAppInstalled() throws InterruptedException {
+
+        final boolean isInstalled = (Boolean)driver.executeScript("mobile: isAppInstalled", params);
+        Assert.assertTrue(isInstalled);
+    }
+//    @Test
+//    public void testAppInBackground() throws InterruptedException {
+//        final boolean wasRunningBefore = (Boolean) driver.executeScript("mobile: terminateApp", params);
+//        Thread.sleep(6000);
+//        Assert.assertTrue(wasRunningBefore);
+//    }
+//    @Test
+//    public void testAppResumed() throws InterruptedException {
+//        driver.executeScript("mobile: activateApp", params);
+//        Thread.sleep(3000);
+//        final long state = (Long) driver.executeScript("mobile: queryAppState", params);
+//        Assert.assertEquals(state, 4);
+//    }
+    @Test
+    public void testAppUninstalled() {
+        driver.removeApp("uxbae.MacAndCheese");
+        final boolean isInstalled = (Boolean)driver.executeScript("mobile: isAppInstalled", params);
+        Assert.assertFalse(isInstalled);
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
+}
 
